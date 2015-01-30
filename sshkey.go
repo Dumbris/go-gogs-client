d41ee524ad806370c204c0f8616db78c9d3d7c9e
Algis Dumbris <a.dumbris@gmail.com>
2015-01-30 13:01:40 +0300

    Introduce structure for ssh Key management. https://github.com/gogits/gogs/issues/878

---
+++ sshkey.go
@@ -0,0 +1,15 @@
+// Copyright 2014 The Gogs Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package gogs
+
+// Key represents a public SSH key used to authenticate a user or deploy script.
+type Key struct {
+	ID    int    `json:"id,omitempty"`
+	Key   string `json:"key,omitempty"`
+	URL   string `json:"url,omitempty"`
+	Title string `json:"title,omitempty"`
+}
+
+//TODO Add functions with getParsedResponse()
