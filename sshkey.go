a2fe1c8ac13d4558e86befc8bb556659fd1d4fef
Algis Dumbris <a.dumbris@gmail.com>
2015-02-03 19:52:06 +0300

    Add functions for list, create, delete user ssh key. https://github.com/gogits/gogs/issues/878

--- sshkey.go
+++ sshkey.go
@@ -4,6 +4,13 @@
 
 package gogs
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Key represents a public SSH key used to authenticate a user or deploy script.
 type Key struct {
 	ID    int    `json:"id,omitempty"`
@@ -17,4 +24,29 @@ type AddSSHKeyOption struct {
 	Key   string `json:"key"`
 }
 
-//TODO Add functions with getParsedResponse()
+// ListMyKeys lists all repositories for the authenticated user that has access to.
+func (c *Client) ListMyKeys() ([]*Key, error) {
+	keys := make([]*Key, 0, 10)
+	err := c.getParsedResponse("GET", "/user/keys", nil, nil, &keys)
+	return keys, err
+}
+
+// CreateKey creates a ssh key for authenticated user.
+func (c *Client) CreateKey(opt AddSSHKeyOption) (*Key, error) {
+	body, err := json.Marshal(&opt)
+	if err != nil {
+		return nil, err
+	}
+	key := new(Key)
+	return key, c.getParsedResponse("POST", "/user/keys",
+		http.Header{"content-type": []string{"application/json"}}, bytes.NewReader(body), key)
+}
+
+func (c *Client) DeleteKey(id int) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("/user/keys/%s", id), nil)
+	if err != nil {
+		return err
+	}
+	_, err = http.DefaultClient.Do(req)
+	return err
+}
